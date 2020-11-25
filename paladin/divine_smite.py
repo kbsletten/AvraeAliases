14ce1e642b8860619c90e5743c475f2c13c9b077
Kyle Sletten <kbsletten@gmail.com>
2020-11-24 21:26:18 -0700

    Add bonus damage for fiends

--- paladin/divine_smite.py
+++ paladin/divine_smite.py
@@ -1,10 +1,11 @@
 embed
 <drac2>
-args = argparse(&ARGS&)
+argv = &ARGS&
+args = argparse(argv)
 command = "&1&"
 spell_level = int(command)
-undead = "undead" in &ARGS&
-critical = "crit" in &ARGS&
+unholy = "undead" in argv or "fiend" in argv
+critical = "crit" in argv
 
 current_combat = combat()
 current_combatant = current_combat.get_combatant(name) if current_combat else None
@@ -18,28 +19,29 @@ description = "You do not have this ability."
 fields = ""
 target_info = ""
 
-damage_roll = vroll(f"{(1 + spell_level + (1 if undead else 0)) * (2 if critical else 1)}d8[magical radiant]")
+damage_roll = vroll(f"{(1 + spell_level + (1 if unholy else 0)) * (2 if critical else 1)}d8[magical radiant]")
 
 if command == "help":
   title = "Divine Smite"
   description = """Starting at 2nd level, when you hit a creature with a melee weapon attack, you can expend one spell slot to deal radiant damage to the target, in addition to the weapon’s damage. The extra damage is 2d8 for a 1st-level spell slot, plus 1d8 for each spell level higher than 1st, to a maximum of 5d8. The damage increases by 1d8 if the target is an undead or a fiend, to a maximum of 6d8."""
-  fields = """-f "Usage|!smite LEVEL [-t TARGET] [undead] [crit]" """
+  fields = """-f "Usage|!smite LEVEL [-t TARGET] [undead|fiend] [crit]" """
 elif has_slot:
     title = f"{combatant_name} uses Divine Smite"
     description = ""
     fields = f"""-f "Meta|**Damage{" (CRIT!)" if critical else ""}**: {damage_roll}" """
     use_slot(spell_level)
-    if current_combat:
-      target_name = args.last("t")
-      target = current_combat.get_combatant(target_name) if current_combat else None
-      if target:
-        damage = damage_roll.consolidated()
-        target_damage = target.damage(damage)["damage"]
-        fields += f"""-f "{target.name}|{target_damage}" """
-        target_info = f"{target.name} {target.hp_str()}\n"
+    target_name = args.last("t")
+    target = current_combat.get_combatant(target_name) if current_combat and target_name else None
+    if target:
+      damage = damage_roll.consolidated()
+      target_damage = target.damage(damage)["damage"]
+      fields += f"""-f "{target.name}|{target_damage}" """
+      target_info = f"{target.name} {target.hp_str()}\n"
     fields+= f"""
 -f "Effect|When you hit a creature with a melee weapon attack, you can expend one spell slot to deal extra radiant damage to the target in addition to the weapon's damage."
 """
+    if unholy:
+      fields += f"""-f "Bonus Damage|The damage increases by 1d8 if the target is an undead or a fiend, to a maximum of 6d8." """
 elif paladin_level >= 2:
   title = f"{combatant_name} tries to use Divine Smite"
   description = "You must finish a long rest before you can use this ability again."
