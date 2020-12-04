b9c071f37489a29b4c92ffbe1b4c528394885c0c
Kyle Sletten <kbsletten@gmail.com>
2020-12-03 23:04:00 -0700

    Refactor favored foe

--- ranger/foe.py
+++ ranger/foe.py
@@ -23,26 +23,28 @@ target_info = ""
 
 if command == "help":
   title = "Favored Foe"
-  fields = """-f "Effect|When you hit a creature with an attack roll, you can call on your mystical bond with nature to mark the target as your favored enemy for 1 minute or until you lose your concentration (as if you were concentrating on a spell).
+  fields += """-f "Usage|**Deal Extra Damage**
+`!init attack shortsword -t TA1 foe`
+**Mark a Favored Foe**
+`!foe -t TA1`" """
+  fields += """-f "Effect|When you hit a creature with an attack roll, you can call on your mystical bond with nature to mark the target as your favored enemy for 1 minute or until you lose your concentration (as if you were concentrating on a spell).
 The first time on each of your turns that you hit the favored enemy and deal damage to it, including when you mark it, you can increase that damage by 1d4.
 You can use this feature to mark a favored enemy a number of times equal to your proficiency bonus, and you regain all expended uses when you finish a long rest.
 This feature’s extra damage increases when you reach certain levels in this class: to 1d6 at 6th level and to 1d8 at 14th level." """
 elif char_has_cc:
   target_name = args.last("t")
   target = init.get_combatant(target_name) if init and target_name else None
-  damage_roll = vroll("1d4" if ranger_level < 6 else "1d6" if ranger_level < 14 else "1d8")
-  fields += f"""-f "Meta|**Damage**: {damage_roll}" """
-  if me and target:
+  fields += f"""-f "Meta|**Effect**: Favored Foe of {name}" """
+  parent = None
+  if me:
     me.add_effect("Favored Foe", "", concentration=True, duration=10)
     parent = me.get_effect("Favored Foe")
-    target.add_effect(f"Favored Foe of {name}", "", parent=parent)
   if target:
-    target.damage(damage_roll.consolidated())
-    target_info = f"{target.name} {target.hp_str()} (-{damage_roll.total})"
+    target.add_effect(f"Favored Foe of {name}", "", parent=parent)
   if char:
     char.mod_cc(cc, -1)
-  fields += """-f "Effect|When you hit a creature with an attack roll, you can call on your mystical bond with nature to mark the target as your favored enemy for 1 minute or until you lose your concentration (as if you were concentrating on a spell).
-The first time on each of your turns that you hit the favored enemy and deal damage to it, including when you mark it, you can increase that damage by 1d4." """
+  fields += f"""-f "Effect|When you hit a creature with an attack roll, you can call on your mystical bond with nature to mark the target as your favored enemy for 1 minute or until you lose your concentration (as if you were concentrating on a spell).
+The first time on each of your turns that you hit the favored enemy and deal damage to it, including when you mark it, you can increase that damage by 1d{4 if ranger_level < 6 else 6 if ranger_level < 14 else 8}." """
   fields += f"""-f "{cc}|{char.cc_str(cc) if char_has_cc else "N/A"} (-1)" """
 </drac2>
 -title "{{title}}"
