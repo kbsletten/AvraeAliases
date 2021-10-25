4ebf6b2fe2b4e7f0ea76b6b8b684c39ea6591237
Kyle Sletten <kbsletten@gmail.com>
2021-10-25 07:59:37 -0600

    Update ex to use durations.

---
+++ dm/ex.md
@@ -0,0 +1,8 @@
+Remove effects from combatants.
+
+**Pick a Duration (default 10m)**
+One hour.
+`!ex -dur 1h`
+
+Two rounds.
+`!ex -dur 2`
--- dm/ex.py
+++ dm/ex.py
@@ -2,6 +2,16 @@ embed
 <drac2>
 args = argparse(&ARGS&)
 
+duration = args.last("dur", "10m")
+rounds = 0
+hours, _, duration = duration.partition("h") if "h" in duration else ["", "", duration]
+minutes, _, duration = duration.partition("m") if "m" in duration else ["", "", duration]
+if hours:
+  rounds += 600 * int(hours)
+if minutes:
+  rounds += 10 * int(minutes)
+rounds += int(duration) if duration else 0
+
 init = combat()
 targets = [(init.get_group(target), init.get_combatant(target)) for target in args.get("t")] if init else []
 if not args.get("t"):
@@ -15,8 +25,10 @@ for group, combatant in targets:
     if each.effects:
       fields += f"""-f "{each.name}|"""
       for effect in each.effects:
-        fields += f"{effect.name}\n"
-        each.remove_effect(effect.name)
+        should_remove = effect.name != "map" and effect.duration > -1 and effect.duration <= duration
+        fields += f"{effect.name}{'' if should_remove else ' (not removed)'}\n"
+        if should_remove:
+          each.remove_effect(effect.name)
       fields += """" """
 </drac2>
 -title "Removing Effects"
