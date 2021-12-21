95741eae6250667b1c55db36f8f592ad94b6e9cf
Kyle Sletten <kbsletten@gmail.com>
2021-12-21 15:40:31 -0700

    Update group name

--- allies/join.py
+++ allies/join.py
@@ -1,3 +1,3 @@
-multiline {{C=character()}}{{A=load_json(C.cvars["ally"] if "ally" in C.cvars else "{}")}}{{N=A["name"] if "name" in A else f"""{C.name}'s {A["type"]} ally"""}}{{I=combat()}}{{M=I.me}}{{G=(M.group if M else None) or ctx.author.display_name}}
+multiline {{C=character()}}{{A=load_json(C.cvars["ally"] if "ally" in C.cvars else "{}")}}{{N=A["name"] if "name" in A else f"""{C.name}'s {A["type"]} ally"""}}{{I=combat()}}{{M=I.me}}{{G=(M.group if M else None) or ctx.author.name}}
 {{f"""!i join -group "{G}" %*% """ if not M else f"""!i opt "{M.name}" -group "{G}" """ if M.group != G else ""}}
 !init madd "{{A["type"]}}" -h False -name "{{N}}" -group "{{G}}" %*%
