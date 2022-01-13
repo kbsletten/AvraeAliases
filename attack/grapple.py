314584c821041a3eef636a40dd818fb8e51c119e
Kyle Sletten <kbsletten@gmail.com>
2022-01-13 09:54:57 -0700

    Fix groups grappling

--- attack/grapple.py
+++ attack/grapple.py
@@ -4,6 +4,8 @@ argv = &ARGS&
 args = argparse(argv)
 init = combat()
 current = init.current if init else character()
+if current and current.type == "group":
+  current = current.combatants[0]
 if args.last("as"):
   current = init.get_combatant(args.last("as")) if init else current
 name = current.name if current else name
